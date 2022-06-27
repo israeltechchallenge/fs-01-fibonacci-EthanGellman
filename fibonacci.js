7ae4bc5919e6e5baae924f1ff58202d6eb6fbed3
EthanGellman <esgellman@hotmail.com>
2022-06-27 12:33:57 -0400

    hi

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,23 @@
-document.getElementById("x").textContent = "1";
-document.getElementById("y").textContent = "1";
+document.getElementById("x").textContent = "2";
 
+z = function() {
+    const x = document.getElementById("x").textContent;
+    a = 0;
+    b = 1;
+    if (x<=1){
+        document.getElementById("y").textContent = x;
+
+    }
+    if (x>1) {
+        for (i = 0;i<x-2;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        document.getElementById("y").textContent = b;
+
+    }
+
+
+}
+z();
