b5b4c12ab48e993ac447ff80afb63c028f17b8d4
EthanGellman <esgellman@hotmail.com>
2022-06-28 13:00:21 -0400

    hi

--- fibonacci.js
+++ fibonacci.js
@@ -1,19 +1,36 @@
-
 z = function(x) {
-
+    document.getElementById("y").textContent = "";
+    document.getElementById("y").style.color = "black";
+    document.getElementById("spinner").classList.remove('opacity-0');
     fetch(x)
         .then(function(response) {
         return response.json();
         })
         .then(function (data) {
+            
+            document.getElementById("spinner").classList.add('opacity-0');
             document.getElementById("y").textContent = data["result"];
-
+        }).catch(err => {
+            console.log(err); // Email is forbidden
+            document.getElementById("spinner").classList.add('opacity-0');
+            document.getElementById("y").textContent = "Server Error: 42 is the meaning of life";
+            document.getElementById("y").style.color = "#D9534F";
         });
 
 
 }
 update = function(){
-    z("http://localhost:5050/fibonacci/"+document.getElementById("x").value.toString());
+    document.getElementById("error_line").textContent = "";
+    document.getElementById("error_line").classList.add('opacity-0');
+    document.getElementById("x").style.color = "black";
+
+    if (document.getElementById("x").value <= 50){
+            z("http://localhost:5050/fibonacci/"+document.getElementById("x").value.toString());
+    }else{
+        document.getElementById("error_line").classList.remove('opacity-0');
+        document.getElementById("x").style.color = "#D9534F";
+        document.getElementById("error_line").textContent = "Can’t be larger than 50";
+
+    }
 
 }
-
