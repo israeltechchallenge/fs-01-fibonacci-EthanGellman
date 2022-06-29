702b7ffb1366ab04abf56c766490d68e479f7234
EthanGellman <esgellman@hotmail.com>
2022-06-29 07:24:54 -0400

    hi

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,24 @@
+get_prev = function(){
+    document.getElementById("spinner2").classList.remove('opacity-0');
+    document.getElementById("first").textContent = "";
+    document.getElementById("second").textContent = "";
+    document.getElementById("third").textContent = "";
+
+    fetch("http://localhost:5050/getFibonacciResults")
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function (data) {
+        data["results"] = data["results"].sort((a, b) => a.createdDate - b.createdDate);
+        last_element = data["results"].length-1;
+        document.getElementById("spinner2").classList.add('opacity-0');
+        document.getElementById("first").textContent = "The Fibonacci of "+ data["results"][last_element]["number"] +" is "+ data["results"][last_element]["result"];
+        document.getElementById("second").textContent = "The Fibonacci of "+ data["results"][last_element-1]["number"] +" is "+ data["results"][last_element-1]["result"];
+        document.getElementById("third").textContent = "The Fibonacci of "+ data["results"][last_element-2]["number"] +" is "+ data["results"][last_element-2]["result"];
+
+    });
+
+}
 z = function(x) {
     document.getElementById("y").textContent = "";
     document.getElementById("y").style.color = "black";
@@ -32,5 +53,6 @@ update = function(){
         document.getElementById("error_line").textContent = "Can’t be larger than 50";
 
     }
-
+    get_prev();
 }
+
